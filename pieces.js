1aa06eff6d161194c849e9ba5ade1caa3b33cf85
Engy Essam <153827798+engy-dev@users.noreply.github.com>
2024-04-01 14:56:53 +0200

    generating first page

---
+++ pieces.js
@@ -0,0 +1,20 @@
+// fetching database of wares
+const reponse = await fetch("pieces-autos.json");
+const pieces = await reponse.json();
+// declaring variables for the content we need to generate 
+const article = pieces[0];
+const imageElement = document.createElement("img");
+imageElement.src = article.image;
+const nomElement = document.createElement("h2");
+nomElement.innerText = article.nom;
+const prixElement = document.createElement("p");
+prixElement.innerText = `Prix: ${article.prix} € (${article.prix < 35 ? "€" : "€€€"})`;
+document.body.appendChild(prixElement);
+const categorieElement = document.createElement("p");
+categorieElement.innerText = article.categorie ?? "(aucune catégorie)";
+// generation of said elements
+const sectionFiches = document.querySelector(".fiches");
+sectionFiches.appendChild(imageElement);
+sectionFiches.appendChild(nomElement);
+sectionFiches.appendChild(prixElement);
+sectionFiches.appendChild(categorieElement);
