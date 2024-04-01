6581a94f5bea9a66f51e09774559f9f509a92f55
Engy Essam <153827798+engy-dev@users.noreply.github.com>
2024-04-01 15:43:07 +0200

    -

--- pieces.js
+++ pieces.js
@@ -3,15 +3,11 @@ const reponse = await fetch("pieces-autos.json");
 const pieces = await reponse.json();
 for (let i = 0; i < pieces.length; i++) {
 
-    // Récupération de l'élément du DOM qui accueillera les fiches
+
     const sectionFiches = document.querySelector(".fiches");
-    // Création d’une balise dédiée à une pièce automobile
     const pieceElement = document.createElement("article");
-    // On crée l’élément img.
     const imageElement = document.createElement("img");
-    // On accède à l’indice i de la liste pieces pour configurer la source de l’image.
     imageElement.src = pieces[i].image;
-    // Idem pour le nom, le prix et la catégorie...
     const nomElement = document.createElement("h2");
 nomElement.innerText = pieces[i].nom;
 const descriptionElement = document.createElement("p");
@@ -30,3 +26,55 @@ sectionFiches.appendChild(categorieElement);
 sectionFiches.appendChild(descriptionElement);
 sectionFiches.appendChild(availablilityElement);
     }
+
+    // sorting of content
+    const boutonTrier = document.querySelector(".btn-trier");
+    boutonTrier.addEventListener("click", function () {
+        const piecesOrdonnees = Array.from(pieces);
+        piecesOrdonnees.sort(function (a, b) {
+            return a.prix - b.prix;
+         });
+         console.log(piecesOrdonnees);
+     });
+
+     const bouton2eTrier = document.querySelector(".btn2e-trier");
+     bouton2eTrier.addEventListener("click", function () {
+        const piecesDecroissant = Array.from(pieces);
+        piecesDecroissant.sort(function (a,b) {
+            return b.prix - a.prix;
+        });
+        console.log(piecesDecroissant);
+     })
+
+     // filtering content
+     const boutonFiltrer = document.querySelector(".btn-filtrer");
+
+     boutonFiltrer.addEventListener("click", function () {
+        const piecesFiltrees = pieces.filter(function (piece) {
+            return piece.prix <= 35;
+        });
+    });
+
+    const bouton2eFiltrer= document.querySelector(".btn2e-filtrer");
+    bouton2eFiltrer = addEventListener("click", function () {
+        const piecesAvecDescription= pieces.filter(function (piece) {
+            return piece.description;
+        })
+        console.log(piecesAvecDescription)
+    })
+    const noms = pieces.map(piece => piece.nom);
+for(let i = pieces.length -1 ; i >= 0; i--){
+   if(pieces[i].prix > 35){
+       noms.splice(i,1)
+   }
+}
+console.log(noms)
+//creating affordable items list 
+const abordablesElements = document.createElement('ul');
+for(let i=0; i < noms.length ; i++){
+   const nomElement = document.createElement('li');
+   nomElement.innerText = noms[i];
+   abordablesElements.appendChild(nomElement)
+}
+document.querySelector('.abordables')
+   .appendChild(abordablesElements)
